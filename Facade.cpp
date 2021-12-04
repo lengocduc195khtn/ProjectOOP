e7b82e20ccf714a422fd0f75a9675fe8affc7687
ThuanDuongHCMUS <94509403+ThuanDuongHCMUS@users.noreply.github.com>
2021-12-04 13:47:30 +0700

    Add files via upload
    Facade + Flight + Hotel + (partially) Transport

---
+++ Facade.cpp
@@ -0,0 +1,109 @@
+#include "Facade.h"
+
+void Facade::showTransports(string location)
+{
+	cout << "List of transports available in" << location << " : \n";
+	for (int i = 0; i < this->_transport.size(); i++)
+		if (this->_transport[i]._location == location) {
+			cout << "Transport type: " << this->_transport[i]._type << endl;//uprade later
+			cout << "ID: " << this->_transport[i]._ID << endl;
+			cout << "Service time: " << this->_transport[i]._time << endl;
+			cout << "------------------------------------------------\n";//seperator
+		}			
+}
+
+void Facade::showHotels(string location)
+{
+	cout << "List of rooms available: \n";
+	for (int i = 0; i < this->_hotel.size(); i++)
+		if (this->_hotel[i]._location == location) {
+			cout << "Hotel: " << this->_hotel[i]._name << "\tRoom left: " << this->_hotel[i].showAvailable() << endl;
+			cout << "------------------------------------------------\n";//seperator
+		}
+			
+}
+
+void Facade::showTickets(string destination)
+{	
+	cout << "List of tickets available: \n";
+	for (int i = 0; i < this->_flight.size(); i++)
+		if (this->_flight[i]._arrivalAirport == destination) {
+			cout << "Flight name: " << this->_flight[i]._name << endl;
+			cout << "Departure airport: " << this->_flight[i]._departureAirport << ": \n";
+			cout << "Number of tickets left: " << this->_flight[i]._seatLeft << endl;;
+			cout << "------------------------------------------------\n";//seperator
+		}			
+}
+
+bool Facade::bookHotel(string location, string name, int amount)
+{
+	for (int i = 0; i < this->_hotel.size(); i++)
+		if (this->_hotel[i]._name == name && this->_hotel[i]._location == location)
+			return this->_hotel[i].book(amount);
+	return false;
+}
+
+bool Facade::bookTicket(string name, int amount, int type)
+{
+	for (int i = 0; i < this->_flight.size(); i++)
+		if (this->_flight[i]._name == name) 
+			return this->_flight[i].book(amount, type);		
+	return false;
+}
+
+bool Facade::bookTransport(string ID)
+{
+	for (int i = 0; i < this->_transport.size(); i++)
+		if (this->_transport[i]._ID == ID)			
+			return this->_transport[i].book();					
+	return false;
+}
+
+bool Flight::book(int amount, int type)
+{
+	if (this->_seatLeft >= amount) {
+		this->_seatLeft -= amount;
+		//create bill
+		return true;
+	}
+	return false;
+}
+
+Flight::Flight(const Flight& ip)
+{
+	this->_arrivalAirport = ip._arrivalAirport;
+	this->_departureAirport = ip._departureAirport;
+	this->_arrivalTime = ip._arrivalAirport;
+	this->_departureTime = ip._departureTime;
+	this->_name = ip._name;
+	this->_seatLeft = ip._seatLeft;
+}
+
+
+bool Ticket::book(int amount, const Flight& ip)
+{
+	if (amount > ip._seatLeft)
+		return false;
+	return true;
+}
+
+bool Hotel::book(int amount)
+{
+	if (this->_roomLeft > amount) {
+		this->_roomLeft -= amount;
+		//create bill
+		return true;
+	}
+	else
+		return false;
+}
+
+int Hotel::showAvailable()
+{
+	return this->_roomLeft;
+}
+
+void Hotel::cancel()
+{
+	this->_roomLeft++;
+}
---
+++ Facade.h
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class Transport;
+class Flight;
+class Hotel;
+class Ticket;
+class Account;
+
+class Facade
+{
+	//all the available flight, hotel, transport
+	vector<Flight> _flight;
+	vector<Hotel> _hotel;
+	vector<Transport> _transport;
+public:
+	void showTransports(string location);
+	void showHotels(string location);
+	void showTickets(string location);
+	bool bookHotel(string location, string name, int amount);
+	bool bookTicket(string name, int amount, int type);	//name = ID
+	bool bookTransport(string ID);
+	void cancelTransports(/*ticket*/);//we need something to valid check
+	void cancelHotel(/*ticket*/);
+	void cancelFlight(/*ticket*/);
+};
+
+class Account {
+	/*vector<bill> _hotels;
+	vector<bill> _flights;
+	vector<bill> _transports;*/
+};
+
+class Transport
+{
+	string _time;
+	string _location;
+	int _type;
+	string _ID;
+public:
+	bool book();
+	friend class Facade;
+};
+
+class Flight
+{
+	string _arrivalAirport;
+	string _departureAirport;
+	string _arrivalTime;
+	string _departureTime;
+	string _name;
+	vector<Ticket> _ticket;
+	int _seatLeft;
+public:
+	friend class Ticket;
+	friend class Facade;
+	bool book(int amount, int type);
+	Flight(const Flight& ip);
+};
+
+class Ticket//not sure what to do with the class now
+{
+	int _fare;
+	bool _isBooked;
+	string _ID;
+public:
+	friend class Facade;
+	friend class Flight;
+	bool book(int amount, const Flight& ip);// book <amount> tickets in flight <ip>
+	//void showAvailable(string time, string destination);
+	//void cancel();
+};
+
+class Hotel
+{
+public:
+	string _name;
+	string _location;
+	int _roomLeft;
+	int _cost;
+
+	bool book(int amount);
+	int showAvailable();
+	void cancel();//update later
+	friend class Facade;
+};
