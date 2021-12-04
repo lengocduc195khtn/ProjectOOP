c78cff994117d5fa83d842c30c0e5869f771435b
Lê Ngọc Đức <93390390+lengocduc195khtn@users.noreply.github.com>
2021-12-04 20:32:45 +0700

    Add files via upload

---
+++ AccountList.h
@@ -0,0 +1,79 @@
+#pragma once
+//#include "Login.h"
+#include "Account.h"
+
+class AccountList
+{
+private:
+    int _size;
+    vector<Account *> _list;
+
+public:
+    AccountList()
+    {
+        this->_size = 0;
+    }
+    ~AccountList()
+    {
+        for (int i = 0; i < this->_size; i++)
+            delete this->_list[i];
+
+        this->_list.clear();
+    }
+
+public:
+    void pushBack(Account *newAccount)
+    {
+        this->_size++;
+        this->_list.push_back(newAccount);
+    }
+
+    Account *find(int position) const
+    {
+        return this->_list[position];
+    }
+
+    int check(string nameUser, bool &checkName, string password, bool &checkPassword) const
+    {
+        checkName = 0;
+
+        for (int i = 0; i < this->_size; i++)
+        {
+            if (this->_list[i]->checkNameUser(nameUser))
+            {
+                checkName = 1;
+                if (this->_list[i]->checkPassword(password))
+                {
+                    checkPassword = 1;
+                    return i;
+                }
+                else
+                {
+                    checkPassword = 0;
+                    return -1;
+                }
+            }
+        }
+        return -1;
+    }
+    bool erase(Account *account)
+    {
+        for (int i = 0; i < this->_size; i++)
+            if (this->_list[i] == account)
+            {
+                this->_list.erase(this->_list.begin() + i);
+                return 1;
+            }
+        return 0;
+    }
+    /*    friend bool Account::deleteAccount(AccountList &accountList)
+        {
+            for (int i = 0; i < accountList._size; i++)
+                if (accountList._list[i] == this)
+                {
+                    accountList._list.erase(accountList._list.begin() + i);
+                    return 1;
+                }
+            return 0;
+        }*/
+};
---
+++ Login.h
@@ -0,0 +1,127 @@
+#include "AccountList.h"
+#include <conio.h>
+
+void inputPassword(string &password)
+{
+    fflush(stdin);
+
+    password = "";
+    int temp;
+    while (temp = getch())
+    {
+        if (temp == 13)
+        {
+            break;
+        }
+        else if (temp == 8)
+        {
+            if (password.length() > 8)
+            {
+                cout << "\b\b";
+                password.erase(password.length() - 1);
+            }
+        }
+        else
+        {
+            cout << "*";
+            password += temp;
+        }
+    }
+    cout << endl;
+}
+
+bool signUp(AccountList *accountList)
+{
+    string nameUser;
+    string password;
+    cout << "Enter your name user: ";
+    cin >> nameUser;
+    cout << "Enter your password: ";
+    // cin >> password;
+    inputPassword(password);
+    Account *temp = new Account(nameUser, password);
+    accountList->pushBack(temp);
+    return 1;
+}
+Account *logIn(const AccountList *accountList)
+{
+    string nameUser;
+    string password;
+    bool checkNameUser = false;
+    bool checkPassword = false;
+    bool func;
+    int posAccount;
+    int checkAgain = 3;
+    cout << "Enter your name user: ";
+    cin >> nameUser;
+    cout << "Enter your password: ";
+    // cin >> password;
+    inputPassword(password);
+
+    while (posAccount = accountList->check(nameUser, checkNameUser, password, checkPassword) && posAccount == -1)
+    {
+        if (checkNameUser == 0)
+        {
+            cout << "Error: name user is not found." << endl;
+            ////
+            /*
+            cout << "Do you want to exit? Press 0. To continue press 1" << endl;
+            cin >> func;
+            if (func == 0)
+                return 0;
+                */
+            ////
+            checkAgain = 3;
+            cout << "Enter your name user: ";
+            cin >> nameUser;
+            cout << "Enter your password: ";
+            // cin >> password;
+            inputPassword(password);
+        }
+        else
+        {
+            cout << "Error: password is incorrect." << endl;
+            ////
+            /*
+            cout << "Do you want to exit? Press 0. To continue press 1" << endl;
+            cin >> func;
+            if (func == 0)
+                return 0;
+                */
+            ////
+            if (--checkAgain)
+            {
+                cout << "Enter your password: ";
+                // cin >> password;
+                inputPassword(password);
+            }
+            else
+            {
+                cout << "You lost too much. Please wait 5 minutes to login again." << endl;
+                ////
+                /*
+                cout << "Do you want to exit? Press 0. To continue press 1" << endl;
+                cin >> func;
+                if (func == 0)
+                    return 0;
+                    */
+                ////
+                checkAgain = 3;
+                Sleep(15000);
+                cout << "Enter your name user: ";
+                cin >> nameUser;
+                cout << "Enter your password: ";
+                // cin >> password;
+                inputPassword(password);
+            }
+        }
+    }
+
+    return accountList->find(posAccount);
+}
+bool logOut(Account **account)
+{
+    *account = NULL;
+    account = NULL;
+    return 1;
+}
