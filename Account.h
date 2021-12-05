ccb4ff161fb73cb070a1432906fd3c4e521bb516
Lê Ngọc Đức <93390390+lengocduc195khtn@users.noreply.github.com>
2021-12-05 18:07:36 +0700

    Add files via upload

---
+++ Account.h
@@ -0,0 +1,95 @@
+#pragma once
+#include "AccountList.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <iomanip>
+#include "json/json.hpp"
+#include <fstream>
+
+#include <deque>
+#include <list>
+#include <unordered_set>
+
+using namespace std;
+using json = nlohmann::json;
+
+class Account
+{
+private:
+    string _nameUser;
+    string _password;
+    /*
+        string _ID;
+        string _name;
+        string _birthdate;
+        string _phone;*/
+    // vector<Hotel*> _hotel;
+    // vector<Tranport*> _tranport;
+    // vector<Flight*> _flight;
+public:
+    void getInfo(json &j)
+    {
+        json k;
+        k["NameUser"] = this->_nameUser;
+        k["Password"] = this->_password;
+
+        j.push_back(k);
+    }
+    /*
+        bool updateInfoUser(string ID, string name, string date, string phone)
+        {
+            this->_ID = ID;
+            this->_name = name;
+            this->_birthdate = date;
+            this->_phone = phone;
+            return 1;
+        }
+        void printinfo()
+        {
+            cout << setw(10) << "Full name:" << this->_name << endl;
+            cout << setw(10) << "Birthdate:" << this->_birthdate << endl;
+            cout << setw(10) << "Phone:" << this->_phone << endl;
+        }*/
+
+public:
+    Account(string nameUser, string password)
+    {
+        this->_nameUser = nameUser;
+        this->_password = password;
+    }
+    ~Account()
+    {
+    }
+    /*
+    bool updateInfoTranport(vector<Tranport*> tranport)
+    {
+    }
+    */
+    bool operator==(const Account *account)
+    {
+        if (account->_nameUser != this->_nameUser || account->_password != this->_password)
+            return 0;
+        return 1;
+    }
+
+public:
+    bool checkNameUser(string nameUser)
+    {
+        return this->_nameUser == nameUser;
+    }
+    bool checkPassword(string password)
+    {
+        return this->_password == password;
+    }
+    void print()
+    {
+        cout << "User:" << this->_nameUser << endl;
+        cout << "Pass:" << this->_password << endl;
+    }
+
+protected:
+    template <class T>
+    bool deleteAccount(T &accountList);
+};
---
+++ AccountList.h
@@ -0,0 +1,132 @@
+#pragma once
+//#include "Login.h"
+#include "Account.h"
+
+class AccountList
+{
+private:
+    int _size;
+    vector<Account *> _list;
+
+public:
+    AccountList()
+    {
+        this->_size = 0;
+    }
+    ~AccountList()
+    {
+        for (int i = 0; i < this->_size; i++)
+            delete this->_list[i];
+
+        this->_list.clear();
+    }
+
+public:
+    void pushBack(Account *newAccount)
+    {
+        this->_size++;
+        this->_list.push_back(newAccount);
+    }
+
+    Account *find(string nameUser) const
+    {
+        for (int i = 0; i < this->_size; i++)
+            if (this->_list[i]->checkNameUser(nameUser))
+                return this->_list[i];
+        return NULL;
+    }
+    bool checkExist(string nameUser)
+    {
+        for (int i = 0; i < this->_size; i++)
+            if (this->_list[i]->checkNameUser(nameUser))
+                return 1;
+        return 0;
+    }
+    bool check(string nameUser, bool &checkName, string password, bool &checkPassword) const
+    {
+        checkName = 0;
+
+        for (int i = 0; i < this->_size; i++)
+        {
+            if (this->_list[i]->checkNameUser(nameUser))
+            {
+                checkName = 1;
+                if (this->_list[i]->checkPassword(password))
+                {
+                    checkPassword = 1;
+                    return 1;
+                }
+                else
+                {
+                    checkPassword = 0;
+                    return 0;
+                }
+            }
+        }
+        return 0;
+    }
+    bool erase(Account *account)
+    {
+        for (int i = 0; i < this->_size; i++)
+            if (this->_list[i] == account)
+            {
+                this->_list.erase(this->_list.begin() + i);
+                return 1;
+            }
+        return 0;
+    }
+    /*    friend bool Account::deleteAccount(AccountList &accountList)
+        {
+            for (int i = 0; i < accountList._size; i++)
+                if (accountList._list[i] == this)
+                {
+                    accountList._list.erase(accountList._list.begin() + i);
+                    return 1;
+                }
+            return 0;
+        }*/
+private:
+    void readFileJson(string address)
+    {
+        json j;
+
+        fstream f;
+        f.open(address, ios::in | ios::binary);
+        f >> j;
+        // iterate the array
+        for (json::iterator it = j.begin(); it != j.end(); ++it)
+        {
+            Account *account = new Account(it->at("NameUser"), it->at("Password"));
+            this->pushBack(account);
+        }
+
+        f.close();
+    }
+    void updateDatabase(string address)
+    {
+        json j;
+        for (int i = 0; i < this->_size; i++)
+        {
+            this->_list[i]->getInfo(j);
+        }
+
+        fstream f;
+        f.open(address, ios::out | ios::binary);
+        f << setw(4) << j;
+        f.close();
+    }
+
+public:
+    void readFile()
+    {
+        this->readFileJson("list.json");
+    }
+
+    // ** Hàm này được sử dụng để cập nhật dữ liệu trong ngày, có thể bao gồm việc thêm, xóa bất kì.
+    // ** Trên thực tế nó không thực tế trong quá trình truy cập thời gian thực (thông tin thay đổi liên tục).
+    // ?? Hàm này tối ưu cho việc xóa dữ liệu từ database
+    void update()
+    {
+        this->updateDatabase("list.json");
+    }
+};
